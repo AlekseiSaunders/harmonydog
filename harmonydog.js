f875328a440ef8b32f49653896b8fa22e1ac31a3
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-21 15:04:43 -0600

    add more boilerplate to harmonydog.js app file

--- harmonydog.js
+++ harmonydog.js
@@ -34,6 +34,58 @@ app.use(
 );
 
 // Load dotenv configurations for use
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 // Load passport configurations for use
+require('./config/passport')(passport);
+
+// Connect to database by invoking function from db.js in config folder
+connectDB();
+
+// Enable morgan in development mode to improve console.logs
+if (process.env.NODE.ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Handlebar helpers will go here when developed
+
+// Declare handlebars as app view engine
+// declare default Layout and extension name for handlebar files
+// will need to bring in handlebar helpers when they are ready
+app.engine(
+  '.hbs',
+  engine({
+    helpers: {},
+    defaultLayout: 'main',
+    extname: '.hbs',
+  })
+);
+
+app.set('view engine', '.hbs');
+
+// Enable session middleware to allow session use (must be declared above passport middleware)
+// store session data in mongo database
+app.use(
+  session({
+    secret: 'funny banana',
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
+
+// Enable passport middleware for authentication and authorization
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Declare static folder to serve static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Declare base routes for use
+app.use('/', mainRoutes);
+
+// Start server
+app.listen(
+  PORT,
+  console.log(`Server is now running on ${PORT} in ${process.env.NODE_ENV}`)
+);
