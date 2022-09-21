6f780e8f9bac130f11c20592f5fdad5bf1279267
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-21 14:50:39 -0600

    add more skeletal structure to harmonydog.js app

--- harmonydog.js
+++ harmonydog.js
@@ -11,3 +11,29 @@ const MongoStore = require('connect-mongo');
 const path = require('path');
 const PORT = process.env.PORT || 1066;
 const app = express();
+
+// link route files for use below
+const mainRoutes = require('./routes/main');
+const authRoutes = require('./routes/auth');
+const { type } = require('os');
+
+// Use body parser middleware to get data from request.body
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Enable method override to allow for updating and deleting via forms
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies for a _method value to activate override, delete _method after capture
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
+// Load dotenv configurations for use
+dotenv.config({path: './config/config.env'});
+
+// Load passport configurations for use
