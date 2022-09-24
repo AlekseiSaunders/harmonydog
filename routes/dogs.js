744cc087172fe254b1ddded34a22a1f9aad88c06
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-24 13:47:46 -0600

    add dog.js route handler

---
+++ routes/dogs.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuth, ensureGuest } = require('../middleware/auth');
+const upload = require('../middleware/multer');
+const dogController = require('../controllers/dog');
+const userController = require('../controllers/user');
+
+// @desc Main landing page
+// @route GET /
+
+router.get('/registerDog', ensureAuth, dogController.getRegister);
+router.post('/registerDog', upload.single('file'), dogController.createDog);
+// router.get('/profile/', ensureAuth, userController.getProfile);
+// router.get('/login', ensureGuest, authController.getLogin);
+// router.post('/login', authController.postLogin);
+// router.get('/logout', authController.logout);
+// router.get('/register', ensureGuest, authController.getRegister);
+// router.post('/register', authController.postRegister);
+
+module.exports = router;
