3d95caf19e25fe4fb4af4a9cd6c9d88a9a98c5c8
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-24 13:45:04 -0600

    add dog controller for dog routes

---
+++ controllers/dog.js
@@ -0,0 +1,36 @@
+const cloudinary = require('../middleware/cloudinary');
+const validator = require('validator');
+const User = require('../models/User');
+const Dog = require('../models/Dog');
+
+exports.getRegister = async (req, res) => {
+  try {
+    const user = await User.find({ user: req.user.id });
+    res.render('registerDog', { user: req.user });
+  } catch (err) {
+    console.err(err);
+  }
+};
+
+exports.createDog = async (req, res) => {
+  try {
+    //upload image to cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+
+    await Dog.create({
+      name: req.body.name,
+      gender: req.body.gender.toLowerCase(),
+      breed: req.body.breed,
+      age: req.body.age,
+      weight: req.body.weight,
+      image: result.secure_url,
+      cloudinaryId: result.public_id,
+      notes: req.body.notes,
+      owner: req.user.id,
+    });
+    console.log('Doggo has been added!');
+    res.redirect('/profile');
+  } catch (err) {
+    console.error(err);
+  }
+};
