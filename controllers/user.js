2b082b97192b5c4c84df9e1ae3a72426ce49c579
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-24 21:56:14 -0600

    add code to pass privileges on to header.ejs

--- controllers/user.js
+++ controllers/user.js
@@ -4,8 +4,13 @@ const Dog = require('../models/Dog');
 exports.getProfile = async (req, res) => {
   try {
     const user = await User.find({ user: req.user.id });
-    const dogs = await Dog.find({ user: req.user.id });
-    res.render('profile', { user: req.user, dogs: dogs });
+    const dogs = await Dog.find({ owner: req.user.id });
+    console.log(user);
+    res.render('profile', {
+      user: req.user,
+      privileges: req.user.privileges,
+      dogs: dogs,
+    });
   } catch (err) {
     console.err(err);
   }
