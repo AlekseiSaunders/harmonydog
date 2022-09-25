24cd527c70d305ed9e06099d306fdd163765f462
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-24 21:56:51 -0600

    add code to end session on logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,5 @@
 const passport = require('passport');
 const validator = require('validator');
-const { deleteOne } = require('../models/User');
 const User = require('../models/User');
 
 exports.getRegister = (req, res) => {
@@ -45,20 +44,16 @@ exports.postLogin = (req, res, next) => {
 };
 
 exports.logout = (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
-    }
-    console.log('User has logged out');
+  req.logout(() => {
+    console.log('User has logged out.');
   });
-  // req.session.destroy((err) => {
-  //   if (err) {
-  //     console.log('Error: Failed to destroy the session during logout', err);
-  //     req.user = null;
-  res.redirect('/');
+  req.session.destroy((err) => {
+    if (err)
+      console.log('Error : Failed to destroy the session during logout.', err);
+    req.user = null;
+    res.redirect('/');
+  });
 };
-//   });
-// };
 
 exports.postRegister = (req, res, next) => {
   const validationErrors = [];
