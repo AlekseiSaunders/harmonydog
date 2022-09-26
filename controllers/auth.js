86e17623e40a3ccb1f3fa939de17ce390fb66f36
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-26 15:01:34 -0600

    change order of routes in authControllers to try and rectify regenerate error

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,6 +20,18 @@ exports.getLogin = (req, res) => {
   }
 };
 
+exports.logout = (req, res) => {
+  req.logout(() => {
+    console.log('User has logged out.');
+  });
+  req.session.destroy((err) => {
+    if (err)
+      console.log('Error : Failed to destroy the session during logout.', err);
+    req.user = null;
+    res.redirect('/');
+  });
+};
+
 exports.postLogin = (req, res, next) => {
   const validationErrors = [];
   if (!validator.isEmail(req.body.userEmail))
