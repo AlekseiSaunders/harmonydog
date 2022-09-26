61295637730a630d1bb9b8c3fc44ece1d6236279
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-26 15:10:17 -0600

    add corrected logout to authControllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -20,15 +20,21 @@ exports.getLogin = (req, res) => {
   }
 };
 
-exports.logout = (req, res) => {
-  req.logout(() => {
+exports.logout = (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
     console.log('User has logged out.');
-  });
-  req.session.destroy((err) => {
-    if (err)
-      console.log('Error : Failed to destroy the session during logout.', err);
-    req.user = null;
-    res.redirect('/');
+    req.session.destroy((err) => {
+      if (err)
+        console.log(
+          'Error : Failed to destroy the session during logout.',
+          err
+        );
+      req.user = null;
+      res.redirect('/');
+    });
   });
 };
 
