a60527d946d11785d4a5ccbac45c93e1726e6b00
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-26 18:24:56 -0600

    add aggregation to dashboard route to display owners and their pets logically

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,21 @@
 const User = require('../models/User');
 const Dog = require('../models/Dog');
+const mongoose = require('mongoose');
+const agg = [
+  {
+    $lookup: {
+      from: 'dogs',
+      localField: '_id',
+      foreignField: 'owner',
+      as: 'pets',
+    },
+  },
+];
 
 exports.getProfile = async (req, res) => {
   try {
     const user = await User.find({ user: req.user.id });
     const dogs = await Dog.find({ owner: req.user.id });
-    console.log(user);
     res.render('profile', {
       user: req.user,
       privileges: req.user.privileges,
@@ -13,5 +23,28 @@ exports.getProfile = async (req, res) => {
     });
   } catch (err) {
     console.err(err);
+  }
+};
+
+exports.getDashboard = async (req, res) => {
+  try {
+    const users = await User.aggregate([
+      {
+        $lookup: {
+          from: 'dogs',
+          localField: '_id',
+          foreignField: 'owner',
+          as: 'pets',
+        },
+      },
+    ]);
+    console.log(users[1].pets[0].name);
+    res.render('dashboard', {
+      user: req.user,
+      users: users,
+      privileges: req.user.privileges,
+    });
+  } catch (err) {
+    console.error(err);
   }
 };
