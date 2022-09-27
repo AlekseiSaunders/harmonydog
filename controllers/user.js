71d3a197065f6d1b43b23828951ca394d7030732
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-27 13:12:58 -0600

    add admin route to take admin straight to dashboard.ejs view

--- controllers/user.js
+++ controllers/user.js
@@ -1,18 +1,35 @@
 const User = require('../models/User');
 const Dog = require('../models/Dog');
 
-
 exports.getProfile = async (req, res) => {
   try {
     const user = await User.find({ user: req.user.id });
     const dogs = await Dog.find({ owner: req.user.id });
-    res.render('profile', {
-      user: req.user,
-      privileges: req.user.privileges,
-      dogs: dogs,
-    });
+    if (req.user.privileges === 'user') {
+      res.render('profile', {
+        user: req.user,
+        privileges: req.user.privileges,
+        dogs: dogs,
+      });
+    } else {
+      const users = await User.aggregate([
+        {
+          $lookup: {
+            from: 'dogs',
+            localField: '_id',
+            foreignField: 'owner',
+            as: 'pets',
+          },
+        },
+      ]);
+      res.render('dashboard', {
+        user: req.user,
+        users: users,
+        privileges: req.user.privileges,
+        dogs: dogs,
+      });
+    }
   } catch (err) {
     console.err(err);
   }
 };
-
