03ddd89f52e579fdb375f3b9e3ed164eaf6a8aa4
Aleksei Saunders <alekseisaunders@gmail.com>
2022-09-26 20:41:27 -0600

    add getAbout and getServices to main controller

--- controllers/main.js
+++ controllers/main.js
@@ -6,3 +6,21 @@ exports.getIndex = (req, res) => {
     res.render('index', { user: req.user, privileges: req.user.privileges });
   }
 };
+
+exports.getAbout = (req, res) => {
+  if (!req.user) {
+    let privileges = 'user';
+    res.render('about', { user: req.user, privileges });
+  } else {
+    res.render('about', { user: req.user, privileges: req.user.privileges });
+  }
+};
+
+exports.getServices = (req, res) => {
+  if (!req.user) {
+    let privileges = 'user';
+    res.render('services', { user: req.user, privileges });
+  } else {
+    res.render('services', { user: req.user, privileges: req.user.privileges });
+  }
+};
