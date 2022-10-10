ec8ef6a248a1316c0e8e1dfc700a634249756265
Aleksei Saunders <alekseisaunders@gmail.com>
2022-10-10 12:03:24 -0600

    add createTip controllers and bring tips into rendering of dashboard

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,10 +1,12 @@
 const User = require('../models/User');
-const Dog = require('../models/Dog');
+const Tip = require('../models/Tip');
+const cloudinary = require('../middleware/cloudinary');
 
 // using mongoDB aggregate to bring in dogs related to the user
 // add them to user document temporarily to combine documents for dashboard view
 exports.getDashboard = async (req, res) => {
   try {
+    const tips = await Tip.find();
     const users = await User.aggregate([
       {
         $lookup: {
@@ -19,8 +21,38 @@ exports.getDashboard = async (req, res) => {
       user: req.user,
       users: users,
       privileges: req.user.privileges,
+      tips: tips,
     });
   } catch (err) {
     console.error(err);
+  }
+};
+
+exports.getCreateTip = (req, res) => {
+  if (!req.user) {
+    let privileges = 'user';
+    res.render('/', { user: req.user, privileges });
+  } else if (req.user.privileges === 'user' || req.user.privileges === 'demo') {
+    req.user.privileges;
+    res.render('/', { user: req.user, privileges });
+  } else {
+    res.render('addTip', { user: req.user, privileges: req.user.privileges });
+  }
+};
+
+exports.postAddTip = async (req, res) => {
+  try {
+    //upload image to cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+    Tip.create({
+      title: req.body.title,
+      body: req.body.body,
+      tags: req.body.tags,
+      image: result.secure_url,
+      cloudinaryId: result.public_id,
+    });
+    res.redirect('/admin/dashboard');
+  } catch (error) {
+    console.error(error);
   }
 };
