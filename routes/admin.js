5a1bf3a9dbceb2f9f495eefe8c1902f0c19de5cc
Aleksei Saunders <alekseisaunders@gmail.com>
2022-10-10 12:03:51 -0600

    add addTip routes to admin.js routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuth } = require('../middleware/auth');
+const upload = require('../middleware/multer');
 
 const adminController = require('../controllers/admin');
 
 // @desc Show Administration Dashboard page
 // @route GET /admin/dashboard
 router.get('/dashboard', ensureAuth, adminController.getDashboard);
+
+// @desc Show addTip page
+// @route GET /admin/addTip
+
+router.get('/addTip', ensureAuth, adminController.getCreateTip);
+
+// @desc Submit Tip page
+// @route POST /admin/addTip
+
+router.post(
+  '/addTip',
+  ensureAuth,
+  upload.single('file'),
+  adminController.postAddTip
+);
 
 module.exports = router;
